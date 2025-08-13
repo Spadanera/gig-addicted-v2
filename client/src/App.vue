9eaf116aa53d6d1deb8768c711767d78bc5738c4
Spadanera <ziro84@gmail.com>
2025-08-13 12:50:44 +0200

    wip

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView, useRoute } from 'vue-router'
 import Axios from '@/services/client'
-import { ref, onBeforeMount, onBeforeUnmount, onMounted } from 'vue'
+import { ref, onBeforeMount } from 'vue'
 import router from '@/router'
 import { UserStore, SnackbarStore, ProgressStore, ThemeStore } from '@/stores'
 import { type User } from '../../models/src'
 import Avatar from './components/Avatar.vue'
+import logo from '@/assets/logo.png'
+import logoInvert from '@/assets/logo-invert.png'
 
 const axios: Axios = new Axios()
 const userStore = UserStore()
@@ -15,6 +17,7 @@ const themeStore = ThemeStore()
 const user = ref<User>({} as User)
 const theme = ref('light')
 const route = useRoute()
+const drawer = ref(false)
 
 function login() {
   user.value = userStore.user
@@ -33,55 +36,51 @@ onBeforeMount(async () => {
   await userStore.checkAuthentication()
   user.value = userStore.user
 })
-
-onMounted(async () => {
-  
-})
-
-onBeforeUnmount(() => {
-  
-})
-
 </script>
 
 <template>
-  <v-responsive class="">
+  <v-responsive>
     <v-app :theme="themeStore.theme">
       <v-app-bar>
         <template v-slot:prepend>
           <RouterLink to="/">
-            <img alt="Gig Addicted" v-if="themeStore.theme === 'light'" class="logo" src="@/assets/logo.png"
-              style="margin-left: 8px; margin-top: 7px;" width="40" height="40" />
-            <img alt="Gig Addicted" v-else class="logo" src="@/assets/logo-invert.png"
-              style="margin-left: 8px; margin-top: 7px;" width="40" height="40" />
+            <img alt="Gig Addicted" :src="themeStore.theme === 'light' ? logo : logoInvert" class="logo" width="40"
+              height="40" style="margin-left: 8px; margin-top: 7px;" />
           </RouterLink>
         </template>
+
+        <v-app-bar-nav-icon @click="drawer = !drawer" class="d-sm-none"></v-app-bar-nav-icon>
+
         <v-app-bar-title>
           <RouterLink to="/">GIG ADDICTED</RouterLink>
         </v-app-bar-title>
-        <RouterLink to="/mybands" v-if="userStore.isLoggedIn">
-          <v-btn variant="text" :class="{ 'text-decoration-underline': route.path.includes('/mybands') }">LE MIE BAND</v-btn>
-        </RouterLink>
-        <RouterLink to="/events">
-          <v-btn variant="plain" :class="{ 'text-decoration-underline': route.path === '/events' }">EVENTI</v-btn>
-        </RouterLink>
-        <RouterLink to="/bands">
-          <v-btn variant="plain" :class="{ 'text-decoration-underline': route.path === '/bands' }">BAND</v-btn>
-        </RouterLink>
-        <RouterLink to="/artists">
-          <v-btn variant="plain" :class="{ 'text-decoration-underline': route.path === '/artists' }">ARTISTI</v-btn>
-        </RouterLink>
-        <v-menu v-if="userStore.isLoggedIn">
+
+        <div class="d-none d-sm-flex">
+          <RouterLink to="/mybands" v-if="userStore.isLoggedIn">
+            <v-btn variant="text" :class="{ 'text-decoration-underline': route.path.includes('/mybands') }">LE MIE
+              BAND</v-btn>
+          </RouterLink>
+          <RouterLink to="/events">
+            <v-btn variant="plain" :class="{ 'text-decoration-underline': route.path === '/events' }">EVENTI</v-btn>
+          </RouterLink>
+          <RouterLink to="/bands">
+            <v-btn variant="plain" :class="{ 'text-decoration-underline': route.path === '/bands' }">BAND</v-btn>
+          </RouterLink>
+          <RouterLink to="/artists">
+            <v-btn variant="plain" :class="{ 'text-decoration-underline': route.path === '/artists' }">ARTISTI</v-btn>
+          </RouterLink>
+        </div>
+
+        <v-menu v-if="userStore.isLoggedIn" class="d-none d-sm-flex">
           <template v-slot:activator="{ props }">
             <v-btn icon v-bind="props">
-              <Avatar :user="userStore.user" alt></Avatar>
+              <Avatar :user="userStore.user" alt />
             </v-btn>
           </template>
-
           <v-list>
             <v-list-item>
               <v-list-item-title>
-                <v-btn @click="themeStore.toggle" v-if="userStore.isLoggedIn" variant="text">
+                <v-btn @click="themeStore.toggle" variant="text">
                   INVERTI TEMA
                   <template v-slot:prepend>
                     <v-icon>{{ theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night' }}</v-icon>
@@ -91,7 +90,7 @@ onBeforeUnmount(() => {
             </v-list-item>
             <v-list-item>
               <v-list-item-title>
-                <v-btn @click="axios.Logout()" v-if="userStore.isLoggedIn" variant="text">
+                <v-btn @click="axios.Logout()" variant="text">
                   ESCI
                   <template v-slot:prepend>
                     <v-icon>mdi-logout</v-icon>
@@ -101,10 +100,57 @@ onBeforeUnmount(() => {
             </v-list-item>
           </v-list>
         </v-menu>
-        <RouterLink to="/login" v-else>
+
+        <RouterLink to="/login" v-else class="d-none d-sm-flex">
           <v-btn style="margin-right: 5px;" text="Accedi" color="primary" variant="outlined" rounded="xl"></v-btn>
         </RouterLink>
       </v-app-bar>
+
+      <v-navigation-drawer v-model="drawer" temporary class="d-sm-none">
+        <v-list>
+          <RouterLink to="/mybands" v-if="userStore.isLoggedIn">
+            <v-list-item>
+              <v-list-item-title>LE MIE BAND</v-list-item-title>
+            </v-list-item>
+          </RouterLink>
+          <RouterLink to="/events">
+            <v-list-item>
+              <v-list-item-title>EVENTI</v-list-item-title>
+            </v-list-item>
+          </RouterLink>
+          <RouterLink to="/bands">
+            <v-list-item>
+              <v-list-item-title>BAND</v-list-item-title>
+            </v-list-item>
+          </RouterLink>
+          <RouterLink to="/artists">
+            <v-list-item>
+              <v-list-item-title>ARTISTI</v-list-item-title>
+            </v-list-item>
+          </RouterLink>
+          <v-divider v-if="userStore.isLoggedIn"></v-divider>
+          <v-list-item v-if="userStore.isLoggedIn">
+            <v-btn @click="themeStore.toggle" variant="text" class="w-full">
+              <v-icon left>{{ theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night' }}</v-icon>
+              INVERTI TEMA
+            </v-btn>
+          </v-list-item>
+          <v-list-item v-if="userStore.isLoggedIn">
+            <v-btn @click="axios.Logout()" variant="text" class="w-full">
+              <v-icon left>mdi-logout</v-icon>
+              ESCI
+            </v-btn>
+          </v-list-item>
+          <RouterLink to="/login" v-else>
+            <v-list-item>
+              <v-list-item-title>
+                <v-btn style="width: 100%" color="primary" variant="outlined" rounded="xl">ACCEDI</v-btn>
+              </v-list-item-title>
+            </v-list-item>
+          </RouterLink>
+        </v-list>
+      </v-navigation-drawer>
+
       <v-main>
         <RouterView v-model="user" @login="login" @reload="reload" />
       </v-main>
@@ -125,7 +171,6 @@ onBeforeUnmount(() => {
       </v-overlay>
     </v-app>
   </v-responsive>
-
 </template>
 
 <style scoped></style>
--- client/src/views/mybands/Band.vue
+++ client/src/views/mybands/Band.vue
@@ -13,7 +13,7 @@ const axios: Axios = new Axios()
 
 const props = defineProps(['band_id'])
 
-const tab = ref(null)
+const tab = ref('details')
 const band = ref<Band>({} as Band)
 const showSetlist = ref(false)
 
@@ -24,7 +24,6 @@ function setBand(b: Band) {
 function toogleShowSetlist() {
     showSetlist.value = !showSetlist.value
 }
-
 </script>
 
 <template>
@@ -34,28 +33,42 @@ function toogleShowSetlist() {
                 <v-btn v-if="$vuetify.display.smAndUp" variant="plain" text="TORNA ALLA LISTA"></v-btn>
                 <v-btn v-else variant="plain" icon="mdi-arrow-left"></v-btn>
             </RouterLink>
-            <v-btn v-if="$vuetify.display.xs && tab === 'setlist'" @click="toogleShowSetlist" icon="mdi-menu"></v-btn>
+
             <v-spacer></v-spacer>
+
             <v-img style="max-width: 30px; max-height: 30px; margin-left: 6px;" v-if="band.logo"
                 :src="band.logo"></v-img>
-            <v-toolbar-title style="flex: none; margin-right: 26px; font-size: medium; margin-left: 10px;" :text="band.name"></v-toolbar-title>
+
+            <v-toolbar-title style="flex: none; margin-right: 26px; font-size: medium; margin-left: 10px;"
+                :text="band.name"></v-toolbar-title>
         </v-toolbar>
-        <v-tabs grow v-model="tab" color="secondary">
-            <v-tab value="details">Dettagli</v-tab>
-            <v-tab value="setlist">Repertorio</v-tab>
-            <v-tab value="event">Eventi</v-tab>
-            <v-tab value="member">Membri</v-tab>
-        </v-tabs>
+
+        <div style="display: flex; align-items: center;">
+            <v-btn v-if="$vuetify.display.xs && tab === 'setlist'" @click="toogleShowSetlist" icon="mdi-menu" variant="plain"
+                style="margin-right: 4px;"></v-btn>
+
+            <v-tabs grow v-model="tab" color="secondary" style="flex: 1;">
+                <v-tab value="details">Dettagli</v-tab>
+                <v-tab value="setlist">Repertorio</v-tab>
+                <v-tab value="event">Eventi</v-tab>
+                <v-tab value="member">Membri</v-tab>
+            </v-tabs>
+        </div>
+
         <v-tabs-window v-model="tab">
             <v-tabs-window-item value="details">
                 <BandDetails @band="setBand" :band_id="props.band_id"></BandDetails>
             </v-tabs-window-item>
+
             <v-tabs-window-item value="setlist">
-                <BandSetlist @tooglesetlist="toogleShowSetlist" :show-setlist="showSetlist" :band_id="props.band_id"></BandSetlist>
+                <BandSetlist @tooglesetlist="toogleShowSetlist" :show-setlist="showSetlist" :band_id="props.band_id">
+                </BandSetlist>
             </v-tabs-window-item>
+
             <v-tabs-window-item value="event">
                 <BandEvent :band_id="props.band_id"></BandEvent>
             </v-tabs-window-item>
+
             <v-tabs-window-item value="member">
                 <BandMember :band_id="props.band_id"></BandMember>
             </v-tabs-window-item>
